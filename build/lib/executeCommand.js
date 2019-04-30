041b1790d9ba1d5eb7f9c88de24c4358fdaaf9e1
dominic.griesel <d.griesel@gmx.net>
2019-04-30 08:21:27 +0200

    Fix breaks

--- build/lib/executeCommand.js
+++ build/lib/executeCommand.js
@@ -4,43 +4,43 @@ const typeguards_1 = require("alcalzone-shared/typeguards");
 const child_process_1 = require("child_process");
 const isWindows = /^win/.test(process.platform);
 function executeCommand(command, argsOrOptions, options) {
-    let args;
-    if (typeguards_1.isArray(argsOrOptions)) {
-        args = argsOrOptions;
-    }
-    else if (typeguards_1.isObject(argsOrOptions)) {
-        // no args were given
-        options = argsOrOptions;
-    }
-    if (options == null)
-        options = {};
-    if (args == null)
-        args = [];
-    const spawnOptions = {
-        stdio: [
-            options.stdin || process.stdin,
-            options.stdout || process.stdout,
-            options.stderr || process.stderr,
-        ],
-        // @ts-ignore This option exists starting with NodeJS 8
-        windowsHide: true,
-    };
-    if (options.cwd != null)
-        spawnOptions.cwd = options.cwd;
-    // Fix npm / node executable paths on Windows
-    if (isWindows) {
-        if (command === "npm")
-            command += ".cmd";
-        else if (command === "node")
-            command += ".exe";
-    }
-    if (options.logCommandExecution == null)
-        options.logCommandExecution = false;
-    if (options.logCommandExecution) {
-        console.log("executing: " + `${command} ${args.join(" ")}`);
-    }
-    // Now execute the npm process and avoid throwing errors
     return new Promise(resolve => {
+        let args;
+        if (typeguards_1.isArray(argsOrOptions)) {
+            args = argsOrOptions;
+        }
+        else if (typeguards_1.isObject(argsOrOptions)) {
+            // no args were given
+            options = argsOrOptions;
+        }
+        if (options == null)
+            options = {};
+        if (args == null)
+            args = [];
+        const spawnOptions = {
+            stdio: [
+                options.stdin || process.stdin,
+                options.stdout || process.stdout,
+                options.stderr || process.stderr,
+            ],
+            // @ts-ignore This option exists starting with NodeJS 8
+            windowsHide: true,
+        };
+        if (options.cwd != null)
+            spawnOptions.cwd = options.cwd;
+        // Fix npm / node executable paths on Windows
+        if (isWindows) {
+            if (command === "npm")
+                command += ".cmd";
+            else if (command === "node")
+                command += ".exe";
+        }
+        if (options.logCommandExecution == null)
+            options.logCommandExecution = false;
+        if (options.logCommandExecution) {
+            console.log("executing: " + `${command} ${args.join(" ")}`);
+        }
+        // Now execute the npm process and avoid throwing errors
         try {
             let bufferedStdout;
             let bufferedStderr;
