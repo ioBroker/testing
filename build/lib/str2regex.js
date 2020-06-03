646cb0350b877afa6ea250e880cf49496b9be5d8
Dominic Griesel <d.griesel@gmx.net>
2020-06-03 09:57:48 +0200

    fix issues found by LGTM.com

--- build/lib/str2regex.js
+++ build/lib/str2regex.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function str2regex(pattern) {
     return new RegExp(pattern
+        .replace(/\\/g, "\\\\") // Backslashes escapen
         .replace(/\./g, "\\.") // Punkte als solche matchen
         .replace(/\*/g, ".*") // Wildcard in Regex umsetzen
         .replace(/!/g, "?!"));
